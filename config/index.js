dc9733f8cb60c674a71381ce05062dfae5e6b9af
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-03-07 23:52:50 +0530

    origin code change

--- config/index.js
+++ config/index.js
@@ -6,6 +6,6 @@ export const API_KEY = process.env.API_KEY;
 export const BASE_URL = process.env.BASE_URL;
 export const PORT = process.env.PORT || 3000;
 export const CORE_OPTIONS = {
-  origin: "https://live-forecast-weahter.netlify.app/",
+  origin: process.env.ORIGIN,
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
 };
